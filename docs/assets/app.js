48e791860c57beca002b4f48c1ae9c8e3f1f0b1a
jinven <fujinwen@yahoo.com>
2022-05-28 13:36:20 +0800

    feat: all and options

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -37,6 +37,49 @@ module.controller('routerCtrl', function () {
   }, false);
 });
 
+  // 所有选项
+  var allOptions = [
+    { name: '全部', code: '全部' },
+    { name: '一', code: '1' },
+    { name: '二', code: '2' },
+    { name: '三', code: '3' },
+    { name: '四', code: '4' },
+    { name: '五', code: '5' }
+  ];
+  // 只有【全部】的选项
+  var oneOptions = allOptions.filter(function(item) {
+    return item.code === '全部';
+  });
+  // 排除【全部】的其他选项
+  var otherOptions = allOptions.filter(function(item) {
+    return item.code !== '全部';
+  });
+// 全部判断
+module.controller('allotherCtrl', function() {
+  var vm = this;
+  // 已选项
+  vm.codes = '';
+  // 下拉项
+  vm.options = [].concat(allOptions);
+  // 下拉选择事件
+  vm.onSelect = function (item, model){
+    console.log('onSelect', item, model, vm.codes);
+    // 选项为【全部】或已选项个数等于其他项个数时，清空下拉项并设置已选项为【全部】
+    if (model === '全部' || otherOptions.length === vm.codes.length) {
+      vm.options = oneOptions;
+      vm.codes = ['全部'];
+    }
+  };
+  // 移除事件
+  vm.onRemove = function (item, model){
+    console.log('onRemove', item, model, vm.codes);
+    // 移除【全部】选项时，显示其他下拉项
+    if (model === '全部') {
+      vm.options = [].concat(allOptions);
+    }
+  };
+});
+
 function toCamelCase(value) {
   var arr = value.split('-').map(function(a) {
     return String(a[0]).toUpperCase() + a.substring(1);
